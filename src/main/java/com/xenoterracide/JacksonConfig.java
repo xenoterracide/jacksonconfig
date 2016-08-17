4150c153769796184abcd0935c9b9a470a11ae2d
Caleb Cushing <xenoterracide@gmail.com>
2016-08-17 08:56:32 -0500

    push jackson2ObjectMapper

--- src/main/java/com/xenoterracide/JacksonConfig.java
+++ src/main/java/com/xenoterracide/JacksonConfig.java
@@ -1,8 +1,5 @@
 package com.xenoterracide;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -12,16 +9,6 @@ class JacksonConfig {
 
     @Bean
     public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-        return new Jackson2ObjectMapperBuilder() {
-
-            @Override
-            public void configure( ObjectMapper objectMapper ) {
-                super.configure( objectMapper );
-                objectMapper.setVisibility( PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE );
-                objectMapper.setVisibility( PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY );
-            }
-
-        };
-
+        return new Jackson2ObjectMapperBuilder().autoDetectFields( true ).autoDetectGettersSetters( false );
     }
 }
